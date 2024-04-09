fe57ba1cf62b66943e24c9deb9ff002018dc2be5
Nursuden <194410008@ogr.kastamonu.edu.tr>
2024-04-09 16:58:09 +0300

    seflink fonksiyonu ile seo url işlemi tamamlandı

--- function.php
+++ function.php
@@ -1,5 +1,21 @@
 <?php
 
+function seflink($text){
+	$find = array("/Ğ/","/Ü/","/Ş/","/İ/","/Ö/","/Ç/","/ğ/","/ü/","/ş/","/ı/","/ö/","/ç/");
+	$degis = array("G","U","S","I","O","C","g","u","s","i","o","c");
+	$text = preg_replace("/[^0-9a-zA-ZÄzÜŞİÖÇğüşıöç]/"," ",$text);
+	$text = preg_replace($find,$degis,$text);
+	$text = preg_replace("/ +/"," ",$text);
+	$text = preg_replace("/ /","-",$text);
+	$text = preg_replace("/\s/","",$text);
+	$text = strtolower($text);
+	$text = preg_replace("/^-/","",$text);
+	$text = preg_replace("/-$/","",$text);
+	return $text;
+}
+echo seflink("şimdi buradaki tüm türkçe ve özel karakterleri düzelt.");
+echo seflink("bulgur pilavı.");
+
 function ekle($table,$gelenveri){ // bu verinin gelmesi lazım artık parametre olarak
 
     global $Connection;
